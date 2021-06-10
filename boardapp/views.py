66bf411984859b78a765c46ae4d48cf00a188f3d
ryoya <ryouya1998.1.21@gmail.com>
2021-06-11 08:19:42 +0900

    update signup func

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.db import IntegrityError
+from django.contrib.auth.models import User
 
 
 def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username, "", password)
+        except IntegrityError:
+            return render(request, "signup.html", {"error": "このユーザーは既に登録されています"})
     return render(request, "signup.html", {"some": 100})
